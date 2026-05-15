60bc393e45ffacc20852a682de5de1826a849840
umayer10-AI <mdumayer10@gmail.com>
2026-05-15 22:12:49 +0600

    add

--- app/src/app/destination/[id]/page.jsx
+++ app/src/app/destination/[id]/page.jsx
@@ -11,7 +11,6 @@ const DetailsPage = async ({params}) => {
     const {token} = await auth.api.getToken({
       headers: await headers()
     })
-    console.log(token)
 
     const {id} = await params
     const p = await getId(id,token)
